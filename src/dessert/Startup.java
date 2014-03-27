b81278567988fdc6fb2fd87b6de700470421a34b
jkemper3 <jkemper3@L11702038596.wctc.msft>
2014-03-27 17:37:44 -0500

    2nd Commit added another object

--- src/dessert/Startup.java
+++ src/dessert/Startup.java
@@ -3,7 +3,6 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package dessert;
 
 import java.text.NumberFormat;
@@ -13,17 +12,23 @@ import java.text.NumberFormat;
  * @author jkemper3
  */
 public class Startup {
+
     public static void main(String[] args) {
-        
-     NumberFormat nf = NumberFormat.getCurrencyInstance();
-        
-      Dessert dessert = new Sundae();
-      dessert = new Sprinkles(dessert);
-      dessert = new HotFudge(dessert);
-      dessert = new Strawberries(dessert);
-      
-      
- System.out.println(dessert.getDescription()+ " " + nf.format(dessert.cost()));
-    
+
+        NumberFormat nf = NumberFormat.getCurrencyInstance();
+
+        Dessert dessert = new Sundae();
+        dessert = new Sprinkles(dessert);
+        dessert = new HotFudge(dessert);
+        dessert = new Strawberries(dessert);
+
+        System.out.println(dessert.getDescription() + " " + nf.format(dessert.cost()));
+
+        Dessert dessert2 = new IceCreamCone();
+        dessert2 = new HotFudge(dessert2);
+        dessert2 = new Sprinkles(dessert2);
+
+        System.out.println(dessert2.getDescription() + " " + nf.format(dessert2.cost()));
+
     }
 }
